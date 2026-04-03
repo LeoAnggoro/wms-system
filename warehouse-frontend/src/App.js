f4bd8555f77f805ea155eb6b6f5fa453cd3ad266
LeoAnggoro <78800542+LeoAnggoro@users.noreply.github.com>
2026-04-03 07:20:12 +0700

    Initial commit atau fix tenant

---
+++ warehouse-frontend/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login'; 
+import Dashboard from './dashboard';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
