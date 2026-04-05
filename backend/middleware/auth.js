9ca9493e82b9cbd1557491fc048df81dc1ec5b1e
LeoAnggoro <78800542+LeoAnggoro@users.noreply.github.com>
2026-04-05 10:27:36 +0700

    perbaikan routes

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,11 +1,13 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const token = req.header("Authorization")?.replace("Bearer ", "");
+  const authHeader = req.header("Authorization");
+  const token = authHeader && authHeader.split(' ')[1];
+
   if (!token) return res.status(401).json({ error: "Akses ditolak" });
 
   try {
-    const verified = jwt.verify(token, process.env.JWT_SECRET || "secret");
+    const verified = jwt.verify(token, process.env.JWT_SECRET || "secret_key");
     req.user = verified;
     next();
   } catch (err) {
