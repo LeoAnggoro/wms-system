b60468f8ad8387940cf952e3dac89d219d48213e
LeoAnggoro <78800542+LeoAnggoro@users.noreply.github.com>
2026-04-05 21:59:04 +0700

    configurasi supabase

--- warehouse-frontend/src/supabaseClient.js
+++ warehouse-frontend/src/supabaseClient.js
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://ajrubsqxqcnblxqjmjsg.supabase.co';
 // GANTI DENGAN ANON KEY DARI SUPABASE DASHBOARD ANDA
 // Settings > API > Project API keys > anon public
-const supabaseAnonKey = 'GANTI_DENGAN_ANON_KEY_ANDA';
+const supabaseAnonKey = 'sb_publishable_9aDmAyLG7K84VIrjnMTGyQ_vPpFYqPt';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
