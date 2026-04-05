c1277b7ea109f1c3e766c9cf90091a09b7b7fb83
LeoAnggoro <78800542+LeoAnggoro@users.noreply.github.com>
2026-04-05 14:12:20 +0700

    Update dashboard.js

--- warehouse-frontend/src/dashboard.js
+++ warehouse-frontend/src/dashboard.js
@@ -118,10 +118,11 @@ const Dashboard = () => {
       data.append('estimatedValue', formData.estimatedValue);
       if (imageFile) data.append('image', imageFile);
 
+      //Test 123
       const config = {
         headers: {
           Authorization: `Bearer ${currentToken}`,
-          'Content-Type': 'multipart/form-data'//test
+          'Content-Type': 'multipart/form-data'
         }
       };
 
