c91fba50a8bed31f93cae4bc0b71083406a2204b
LeoAnggoro <78800542+LeoAnggoro@users.noreply.github.com>
2026-04-05 10:19:08 +0700

    perbaikan untuk routes

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,14 +1,14 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const token = req.header("Authorization")?.split(" ")[1]; // Ambil token dari header
-  if (!token) return res.status(401).json("Akses ditolak");
+  const token = req.header('Authorization')?.split(' ')[1];
+  if (!token) return res.status(401).json({ error: 'Access Denied' });
 
   try {
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = verified; // Di sini req.user diisi data dari token
+    const verified = jwt.verify(token, process.env.JWT_SECRET || 'secretkey');
+    req.user = verified;
     next();
   } catch (err) {
-    res.status(400).json("Token tidak valid");
+    res.status(400).json({ error: 'Invalid Token' });
   }
 };
